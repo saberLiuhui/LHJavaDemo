d6a401851acea876b69dd6a3e546a9cb3cd05c85
Liu,Hui(MBBD) <liuhui34@baidu.com>
2021-03-10 13:40:41 +0800

    替换java文件

---
+++ Example/Pods/Target Support Files/LHJavaDemo/LHJavaDemo-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_LHJavaDemo : NSObject
+@end
+@implementation PodsDummy_LHJavaDemo
+@end
---
+++ Example/Pods/Target Support Files/Pods-LHJavaDemo_Example/Pods-LHJavaDemo_Example-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_LHJavaDemo_Example : NSObject
+@end
+@implementation PodsDummy_Pods_LHJavaDemo_Example
+@end
